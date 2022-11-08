fba873641f89a0fcb14001a0485cca7aedfe8eed
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-11-08 17:59:30 +0600

    destination by id route is created

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 const uri = 'mongodb://localhost:27017';
 
 const client = new MongoClient(uri, {
@@ -15,20 +18,31 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-app.use(cors());
-app.use(express.json());
-
 app.get('/', (_req, res) => {
   res.send('Home Page');
 });
 
 async function connectDb() {
   const Destinations = client.db('Adventurer').collection('Destinations');
+  const Reviews = client.db('Adventurer').collection('Reviews');
 
-  app.get('/destinations', async (req, res) => {
+  app.get('/destinations', async (_req, res) => {
     const cursor = Destinations.find({});
     const destinations = await cursor.toArray();
     res.json(destinations);
+  });
+
+  app.get('/destinations-for-home', async (_req, res) => {
+    const cursor = Destinations.find({}).sort({ time: -1 }).limit(3);
+    const destinations = await cursor.toArray();
+    res.json(destinations);
+  });
+
+  app.get('/destinations/:id', async (req, res) => {
+    const id = req.params.id;
+    const cursor = Destinations.find({ _id: ObjectId(id) });
+    const destination = await cursor.toArray();
+    res.json(destination);
   });
 }
 
